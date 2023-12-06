acc77c907a2cb2377e4f05743b60f23b79998c77
Cuong Le <cuong.le@doctoranywhere.com>
2023-12-06 23:39:44 +0700

    update code

--- customer/src/test/java/learning/resilient/customer/controller/CustomerControllerTest.java
+++ customer/src/test/java/learning/resilient/customer/controller/CustomerControllerTest.java
@@ -1,23 +1,19 @@
 package learning.resilient.customer.controller;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import java.util.concurrent.CompletableFuture;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.result.StatusResultMatchers;
-
-import java.util.concurrent.CompletableFuture;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -69,7 +65,7 @@ class CustomerControllerTest {
   @Test
   public void test_CBClosed_bulkheadfull() throws Exception {
     // use slow calls block exhaust bulkhead
-    var bulkhead = 5;
+    var bulkhead = 3;
     for (var i = 0; i < bulkhead; i++) {
       CompletableFuture.supplyAsync(
           () -> {
@@ -91,7 +87,7 @@ class CustomerControllerTest {
             MockMvcResultMatchers.jsonPath("code", CoreMatchers.is("BulkheadFullException")));
   }
 
-    @Test
+//    @Test
     public void test_timeout() throws Exception {
         // call slow again -> Bulkhead triggers
         mockMvc
@@ -99,48 +95,5 @@ class CustomerControllerTest {
                 .andExpect(MockMvcResultMatchers.status().is5xxServerError())
                 .andExpect(
                         MockMvcResultMatchers.jsonPath("code", CoreMatchers.is("TimeoutException")));
-    }
-
-    @Test
-    public void test_both_CBopen_semaphoreBulkheadFull_CB_first() throws Exception {
-//        var cbOpen = 5;
-//        for (var i = 0; i < cbOpen; i++) {
-//            mockMvc.perform(get("/v1/customers/tech-error"));
-//        }
-//
-//        // CB now OPEN
-//        assertThat(
-//                circuitBreakerRegistry.circuitBreaker("order").getState(),
-//                CoreMatchers.is(CircuitBreaker.State.OPEN));
-//
-//        // CB now HALF_OPEN -> allow 3 calls
-//        Thread.sleep(6000);
-//        assertThat(
-//                circuitBreakerRegistry.circuitBreaker("order").getState(),
-//                CoreMatchers.is(CircuitBreaker.State.HALF_OPEN));
-
-        // use slow calls block exhaust bulkhead
-        var bulkhead = 5;
-        for (var i = 0; i < bulkhead; i++) {
-            CompletableFuture.supplyAsync(
-                    () -> {
-                        try {
-                            return mockMvc.perform(get("/v1/customers/slow/annotated"));
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-        }
-        Thread.sleep(1000);
-
-        mockMvc
-                .perform(get("/actuator/bulkheadevents")).andDo(MockMvcResultHandlers.print());
-
-        // call slow again -> CB checks first
-        mockMvc
-                .perform(get("/v1/customers/slow/annotated"))
-                .andExpect(MockMvcResultMatchers.status().is5xxServerError())
-                .andExpect(
-                        MockMvcResultMatchers.jsonPath("code", CoreMatchers.is("CallNotPermittedException")));
     }
 }
